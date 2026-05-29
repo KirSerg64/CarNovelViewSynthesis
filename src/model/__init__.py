76641c3cd36f5b9acf2ff6e346e635c3e1883aab
Kirill Kobychev <k.kobychev@atlsar.com>
2026-05-29 16:29:24 +0300

    train IFNet with depth integration

---
+++ src/model/__init__.py
@@ -0,0 +1,5 @@
+from src.model.RIFE import RifeModel
+from model.IFNet import IFNet
+from model.IFNet_m import IFNet_m
+from src.model.loss import EPE, LapLoss, SOBEL
+from model.warplayer import warp    
